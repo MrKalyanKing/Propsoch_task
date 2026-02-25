19891b2a7e2f3cf472a3f6afa54ef87ac0b38a94
MrKalyanKing <badhavathkalyan08@gmail.com>
2026-02-25 09:05:40 +0530

    Add service to calculate user balances based on expenses and participants.

--- src/services/balance.service.js
+++ src/services/balance.service.js
@@ -3,37 +3,40 @@ import pool from "../db/db"
 const getUserBalance = async (userId) => {
 
 
-
-    const [userexpense] = pool.query(
-        `select E.id as expense_id
+    try {
+        const [userexpense] = pool.query(
+            `select E.id as expense_id
         E.paid_by,
         Ep.user_id,
         Ep.share_amount
         from expense_participants Ep
         join expenses E on ep.expense_id = e.id
         where ep.user_id= ? `,
-        [userId]
-    )
+            [userId]
+        )
 
-    const balance = {}
+        const balance = {}
 
-    for (const row of userexpense) {
-        const payer = row.paid_by
-        const participant = row.user_id
-        const share = row.share_amount
+        for (const row of userexpense) {
+            const payer = row.paid_by
+            const participant = row.user_id
+            const share = row.share_amount
 
-        const amount = Number(share)
+            const amount = Number(share)
 
-        // if user paid , others owe me
+            // if user paid , others owe to user
 
-        if (payer === userId && participant !== userId) {
-            balance[participant] = (balance[participant] || 0) + amount
+            if (payer === userId && participant !== userId) {
+                balance[participant] = (balance[participant] || 0) + amount
+            }
+
+            //user is  participates , user owes to other
+            else if (participant === userId && payer !== userId) {
+                balance[payer] = (balance[payer] || 0) - amount
+            }
         }
-
-        //user participates , owe payer
-        else if (participant === userId && payer !== userId) {
-            balance[payer] = (balance[payer] || 0) - amount
-        }
+    } catch (err) {
+        return res.status(400).json({ message: "Error in the fetching the expense", err: err })
     }
 
     return balance
